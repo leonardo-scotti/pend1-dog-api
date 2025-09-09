93700d96437f94a22345cbb87163fc177b88e5a1
LEONARDO SCOTTI TOBIAS <25132821@CFP127ALUNO.LOCAL>
2025-09-09 16:30:15 -0300

    Consumindo api de Cachorros

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+const searching = document.getElementById('searching');
+
+async function searchingDog() {
+    const main = document.getElementById('main');
+    const search = document.getElementById('search').value;
+
+    main.innerHTML = ''
+
+    const photos = await buscarImagens(search);
+
+    photos.forEach((url) => {
+        const img = document.createElement('img');
+        img.src = url;
+
+        main.appendChild(img);
+    });
+
+    console.log(search)
+
+}
+
+async function buscarImagens(breed) {
+    const url = `https://dog.ceo/api/breed/${breed}/images`;
+    const response = await fetch(url);
+    const photos = await response.json();
+    console.log
+    return photos.message;
+};
+
+searching.addEventListener('click', searchingDog)
